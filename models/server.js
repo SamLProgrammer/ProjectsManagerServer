9e5834514391910c09c4b9fcc22b7a6063f12a21
SamLProgrammer <samuel.lopez@uptc.edu.co>
2022-05-26 21:44:34 -0500

    server base implemented

---
+++ models/server.js
@@ -0,0 +1,38 @@
+const express = require ('express');
+const cors = require ('cors');
+const PORT = 4000;
+class Server {
+    constructor() {
+        this.app = express();
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
+        this.port = PORT;
+        this.middleware();
+        this.routes();
+        this.sockets();
+    }
+
+    middleware() {
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+        this.app.use('/', require('../routes/routes'));
+    }
+
+    sockets() {
+        this.io.on('connection', (socket) => {
+            console.log('Client Connected!');
+        })
+    }
+
+    listen() {
+        this.server.listen(this.port, () => {
+            console.log('Server Listening on PORT: ' + PORT)
+        })
+    }
+}
+
+module.exports = Server;
