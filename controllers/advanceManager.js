f477b74fb6f2018328c7e08b9561f34666d45df2
SamLProgrammer <samuel.lopez@uptc.edu.co>
2022-07-18 21:35:04 -0500

    date format changed using moment library on advances

--- controllers/advanceManager.js
+++ controllers/advanceManager.js
@@ -14,8 +14,6 @@ class AdvancesManager {
     editAdvance(advance_info, res) {
       //validate not previous date from today for the advance edition
       const advance_id = advance_info.advance_id;
-      const advance_initial_hour = advance_info.initial_hour;
-      const advance_final_hour = advance_info.final_hour;
       const advance_comments = advance_info.comments;
 
       this.db_connection.query("SELECT * FROM ADVANCE WHERE Advance_Id = " + advance_id, (err, result, fields) => {
@@ -23,8 +21,11 @@ class AdvancesManager {
         } else {
           const activity_id = result[0].Activity_Id;
           const user_id = result[0].User_Id;
-          
-          const insertion_query = "REPLACE INTO ADVANCE (Advance_Id, Activity_Id, User_Id, Advance_Comments, Initial_Time, Final_Time) VALUES (" + advance_id +", " + activity_id + ", " + user_id + ", '" + advance_comments +"', " + advance_initial_hour + ", " + advance_final_hour + "))";
+
+          const initial_hour = this.moment(new Date(advance_info.initial_hour)).format("YYYY-MM-DD hh:mm:ss");
+          const final_hour = this.moment(new Date(advance_info.final_hour)).format("YYYY-MM-DD hh:mm:ss");
+
+          const insertion_query = "REPLACE INTO ADVANCE (Advance_Id, Activity_Id, User_Id, Advance_Comments, Initial_Time, Final_Time) VALUES (" + advance_id +", " + activity_id + ", " + user_id + ", '" + advance_comments +"', '" + initial_hour + "', '" + final_hour + "'))";
           this.db_connection.query(insertion_query, function (err, result, fields) {
             if (err) throw err
             res.send(result);
@@ -49,13 +50,12 @@ class AdvancesManager {
 
     createAdvance(advance_info, res) {
 
-        const advance_initial_hour = advance_info.initial_hour;
-        const advance_final_hour = advance_info.final_hour;
         const advance_comments = advance_info.comments;
         const activity_id = advance_info.activity_id;
         const user_id = advance_info.user_id;
-
-        const insertion_query = "INSERT INTO ADVANCE (Activity_Id, User_id, Advance_Comments,  Initial_Time, Final_Time) VALUES (" + activity_id + ", " + user_id + ", '" + advance_comments + "', " + advance_initial_hour + ", " + advance_final_hour + "))";
+        const initial_hour = this.moment(new Date(advance_info.initial_hour)).format("YYYY-MM-DD hh:mm:ss");
+        const final_hour = this.moment(new Date(advance_info.final_hour)).format("YYYY-MM-DD hh:mm:ss");
+        const insertion_query = "INSERT INTO ADVANCE (Activity_Id, User_id, Advance_Comments,  Initial_Time, Final_Time) VALUES (" + activity_id + ", " + user_id + ", '" + advance_comments + "', '" + initial_hour + "', '" + final_hour + "'))";
         this.db_connection.query(insertion_query, function (err, result, fields) {
             if (err) throw err
             res.send('1 Advance Inserted!');
