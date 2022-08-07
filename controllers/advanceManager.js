b99ee8a40a054547ae2a4b6bb4a6bb7d6f2ff86f
SamLProgrammer <samuel.lopez@uptc.edu.co>
2022-08-06 23:22:24 -0500

    fixing advance creation and edit

--- controllers/advanceManager.js
+++ controllers/advanceManager.js
@@ -17,22 +17,14 @@ class AdvancesManager {
       const advance_id = advance_info.advance_id;
       const advance_comments = advance_info.comments;
 
-      this.db_connection.query("SELECT * FROM ADVANCE WHERE Advance_Id = " + advance_id, (err, result, fields) => {
-        if (err) { throw err
-        } else {
-          const activity_id = result[0].Activity_Id;
-          const user_id = result[0].User_Id;
-
           const initial_hour = this.moment(new Date(advance_info.initial_hour)).format("YYYY-MM-DD hh:mm:ss");
           const final_hour = this.moment(new Date(advance_info.final_hour)).format("YYYY-MM-DD hh:mm:ss");
 
-          const insertion_query = "REPLACE INTO ADVANCE (Advance_Id, Activity_Id, User_Id, Advance_Comments, Initial_Time, Final_Time) VALUES (" + advance_id +", " + activity_id + ", " + user_id + ", '" + advance_comments +"', '" + initial_hour + "', '" + final_hour + "')";
+          const insertion_query = "REPLACE INTO ADVANCE (Advance_Id, Advance_Comments, Initial_Time, Final_Time) VALUES (" + advance_id + ", '" + advance_comments + "', '" + initial_hour + "', '" + final_hour + "')";
           this.db_connection.query(insertion_query, function (err, result, fields) {
             if (err) throw err
             res.send(result);
           });
-        }
-      });
     }
 
     getAdvancesByUser(advance_info, res) {
