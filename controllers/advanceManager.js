39e1e1917c2161905c661019edeee4dbdfc95b18
SamLProgrammer <samuel.lopez@uptc.edu.co>
2022-08-11 02:36:44 -0500

    distributed advance case handling v1

--- controllers/advanceManager.js
+++ controllers/advanceManager.js
@@ -12,35 +12,95 @@ class AdvancesManager {
     });
   }
 
-  firstAdvanceValidation(advance_info, res) {
-    const initial_time = this.moment(new Date(advance_info.initial_hour));
-    const final_time = this.moment(new Date(advance_info.final_hour));
+  async firstAdvanceValidation(advance_info, res) {
+    try {
+      const user_id = advance_info.user_id;
+      const activity_id = advance_info.activity_id;
+      const initial_time = this.moment(new Date(advance_info.initial_hour));
+      const final_time = this.moment(new Date(advance_info.final_hour));
 
-    let first_limit = this.moment(new Date(advance_info.initial_hour));
-    let second_limit = this.moment(new Date(advance_info.initial_hour));
+      const advance_minutes = final_time.diff(initial_time,'minutes');
 
-    console.log('advance lasts: ' + initial_time.diff(final_time, 'hours') + ' hours')
-    first_limit.set('hour', 8);
-    first_limit.set('minute', 0);
-    first_limit.set('second', 0);
-    second_limit.set('hour', 18);
-    second_limit.set('minute', 0);
-    second_limit.set('second', 0);
-    if(( initial_time > final_time )
-     || (final_time.diff(initial_time, 'hours') < 8 && ( initial_time < first_limit || final_time > second_limit))) {
-      console.log('entre al que no era 1');
-      res.send({time_off: 'reversedTimes'});
-    } else if (final_time.diff(initial_time, 'hours') > 8 && initial_time > first_limit){
-      console.log('entré acá');
-      this.getAdvances("SELECT * FROM user").then((result) => console.log(result)).catch((err) => console.log(err));
-      res.send({oli: 'oli'});
-    }else {
-      console.log('entre al que no era 2');
-      this.validateAndCreateAdvance(advance_info, res);
+      let first_limit = this.moment(new Date(advance_info.initial_hour));
+      let second_limit = this.moment(new Date(advance_info.initial_hour));
+
+      console.log('advance lasts: ' + final_time.diff(initial_time, 'hours') + ' hours')
+      console.log('advance lasts: ' + advance_minutes + ' minutes')
+      first_limit.set('hour', 8);
+      first_limit.set('minute', 0);
+      first_limit.set('second', 0);
+      second_limit.set('hour', 18);
+      second_limit.set('minute', 0);
+      second_limit.set('second', 0);
+      if ((initial_time > final_time)
+        || (final_time.diff(initial_time, 'hours') < 8 && (initial_time < first_limit || final_time > second_limit))) {
+        console.log('entre al que no era 1');
+        res.send({ time_off: 'reversedTimes' });
+      } else if (final_time.diff(initial_time, 'hours') > 8 && initial_time > first_limit) {
+        const activity_assignment = await this.dynamicQuery("SELECT * from activity_assignment WHERE User_Id = " + user_id + " AND Activity_Id = " + activity_id);
+        this.dynamicQuery("SELECT * FROM advance WHERE Activity_Assignment_Id IN (SELECT Activity_Assignment_Id  FROM Activity_Assignment WHERE User_Id = " + user_id + ") AND Initial_Time >= '" + initial_time.format("YYYY-MM-DD HH:mm:ss") + "' AND Final_Time < '" + this.moment(new Date(activity_assignment[0].Final_Time)).format("YYYY-MM-DD HH:mm:ss") + "'").then(
+          async (result) => {
+
+            result.sort((a, b) => { return this.moment(new Date(a.Initial_Time)) - this.moment(new Date(b.Initial_Time)) });
+            const overlapping_advance = await this.dynamicQuery("SELECT * FROM ADVANCE WHERE Initial_Time <= '" + initial_time.format("YYYY-MM-DD HH:mm:ss") + "' AND Final_Time >= '" + initial_time.format("YYYY-MM-DD HH:mm:ss") + "'");
+
+            let first_pointer = (overlapping_advance[0].Initial_Time) ? this.moment(new Date(overlapping_advance[0].Final_Time)) : this.moment(new Date(advance_info.initial_hour)); // Cambiar esto
+            let current_date = (overlapping_advance[0].Initial_Time) ? this.moment(new Date(overlapping_advance[0].Final_Time)) : this.moment(new Date(advance_info.initial_hour));;
+
+            let third_pointer = this.moment(new Date(advance_info.initial_hour));
+            third_pointer.set('hour', 18);
+            third_pointer.set('minute', 0);
+            third_pointer.set('second', 0);
+            let times_list = [];
+
+            while (current_date <= this.moment(new Date(activity_assignment[0].Final_Time))) {
+              let day_advances = [];
+              result.forEach(element => {
+                if (this.moment(new Date(element.Initial_Time)).isSame(current_date.format('YYYY-MM-DD'), 'day')) {
+                  day_advances.push(element);
+                }
+              });
+              if (day_advances.length == 0) {
+                //substract times and remaining minutes on advance total time
+                times_list.push({ i_time: first_pointer.format("YYYY-MM-DD HH:mm:ss"), f_time: third_pointer.format("YYYY-MM-DD HH:mm:ss") });
+              } else {
+                while (day_advances.length > 0) {
+                  let current_advance = day_advances.shift();
+                  let second_pointer = this.moment(new Date(current_advance.Initial_Time));
+                  if (second_pointer.diff(first_pointer, 'minutes') > 0) {
+                    //substract times and remaining minutes on advance total time
+                    times_list.push({ i_time: first_pointer.format("YYYY-MM-DD HH:mm:ss"), f_time: second_pointer.format("YYYY-MM-DD HH:mm:ss") });
+                  }
+                  first_pointer = this.moment(new Date(current_advance.Final_Time));
+                  if(day_advances.length == 0 && third_pointer.diff(first_pointer, 'minutes') > 0) {
+                    //substract times and remaining minutes on advance total time
+                    times_list.push({ i_time: first_pointer.format("YYYY-MM-DD HH:mm:ss"), f_time: third_pointer.format("YYYY-MM-DD HH:mm:ss") });
+                  }
+                }
+              }
+              console.log(current_date.add(1, 'days'));
+              first_pointer = this.moment(current_date);
+              third_pointer = this.moment(current_date);
+              first_pointer.set('hour', 8);
+              first_pointer.set('minute', 0);
+              first_pointer.set('second', 0);
+              third_pointer.set('hour', 18);
+              third_pointer.set('minute', 0);
+              third_pointer.set('second', 0);
+            }
+            console.log(times_list);
+            res.send(times_list);
+          }).catch((err) => console.log(err));
+      } else {
+        console.log('entre al que no era 2');
+        this.validateAndCreateAdvance(advance_info, res);
+      }
+    } catch (err) {
+      console.log(err);
     }
   }
 
-  getAdvances(query_string) {
+  dynamicQuery(query_string) {
     return new Promise((resolve, reject) => {
       this.db_connection.query(query_string, (err, result, fields) => {
         if (err) {
@@ -80,8 +140,8 @@ class AdvancesManager {
             }
           }
         });
-        if(flag) {
-          res.send({overlapped: true});
+        if (flag) {
+          res.send({ overlapped: true });
         }
         else {
           const insertion_query_1 = "SELECT * FROM Activity_Assignment WHERE Activity_Id = " + activity_id + " AND User_Id = " + user_id;
@@ -137,10 +197,10 @@ class AdvancesManager {
                               this.db_connection.query(insertion_query_7, function (err6, result6, fields6) {
                                 if (err6) {
                                   console.log(err6);
-                                } else  {
-                                  if((remaining_hours_for_activity > future_free_time)) {
+                                } else {
+                                  if ((remaining_hours_for_activity > future_free_time)) {
                                     this.db_connection.query("DELETE FROM ADVANCE WHERE Advance_Id = " + result6.insertId, (err12, result12, fields12) => {
-                                      if(err12) {
+                                      if (err12) {
                                         throw err12;
                                       }
                                     });
@@ -195,82 +255,82 @@ class AdvancesManager {
                 }
               }
             });
-            if(flag) {
-              res.send({overlapped: true});
+            if (flag) {
+              res.send({ overlapped: true });
             }
             else {
-            const insertion_query_1 = "SELECT * FROM Activity_Assignment WHERE Activity_Id = " + result0[0].Activity_Id + " AND User_Id = " + result0[0].User_Id;
-            this.db_connection.query(insertion_query_1, (err1, result1, fields1) => {
-              if (err1) {
-                console.log(err1);
-              } else {
-                const insertion_query_2 = "SELECT * FROM ACTIVITY WHERE Activity_Id IN (SELECT Activity_Id FROM Activity_Assignment WHERE Activity_Assignment_Id = " + result1[0].Activity_Assignment_Id + ")";
-                this.db_connection.query(insertion_query_2, (err2, result2, fields2) => {
-                  if (err2) {
-                    console.log(err2)
-                  } else {
-                    const insertion_query_3 = "SELECT * FROM ADVANCE WHERE Initial_Time > '" + this.moment(new Date(result1[0].Initial_Time)).format("YYYY-MM-DD HH:mm:ss") + "' AND Final_Time < '" + this.moment(new Date(result1[0].Final_Time)).format("YYYY-MM-DD HH:mm:ss") + "' AND Activity_Assignment_Id = " + result1[0].Activity_Assignment_Id;
-                    this.db_connection.query(insertion_query_3, (err3, result3, fields3) => {
-                      if (err3) {
-                        console.log(err3)
-                      } else {
-                        let invested_hours_on_activity = 0;
-                        result3.forEach(element => {
-                          let diff = Math.abs(element.Final_Time - element.Initial_Time);
-                          invested_hours_on_activity += diff / 3600000;
-                        });
-                        const remaining_hours_for_activity = result2[0].Estimated_Hours - invested_hours_on_activity;
-                        const a = this.moment(new Date());
-                        const b = this.moment(result1[0].Final_Time);
-                        const c = b.diff(a, 'days');
-                        console.log(c);
-                        const initial_weeknd = (6 - a.day() > c) ? 0 : 1;
-                        let weekends_amount = (parseInt((c - (6 - a.day())) / 7) + initial_weeknd) * 2;
-                        weekends_amount = (b.day() == 5) ? weekends_amount - 1 : weekends_amount;
-                        const working_hours = (c - weekends_amount) * 8;
+              const insertion_query_1 = "SELECT * FROM Activity_Assignment WHERE Activity_Id = " + result0[0].Activity_Id + " AND User_Id = " + result0[0].User_Id;
+              this.db_connection.query(insertion_query_1, (err1, result1, fields1) => {
+                if (err1) {
+                  console.log(err1);
+                } else {
+                  const insertion_query_2 = "SELECT * FROM ACTIVITY WHERE Activity_Id IN (SELECT Activity_Id FROM Activity_Assignment WHERE Activity_Assignment_Id = " + result1[0].Activity_Assignment_Id + ")";
+                  this.db_connection.query(insertion_query_2, (err2, result2, fields2) => {
+                    if (err2) {
+                      console.log(err2)
+                    } else {
+                      const insertion_query_3 = "SELECT * FROM ADVANCE WHERE Initial_Time > '" + this.moment(new Date(result1[0].Initial_Time)).format("YYYY-MM-DD HH:mm:ss") + "' AND Final_Time < '" + this.moment(new Date(result1[0].Final_Time)).format("YYYY-MM-DD HH:mm:ss") + "' AND Activity_Assignment_Id = " + result1[0].Activity_Assignment_Id;
+                      this.db_connection.query(insertion_query_3, (err3, result3, fields3) => {
+                        if (err3) {
+                          console.log(err3)
+                        } else {
+                          let invested_hours_on_activity = 0;
+                          result3.forEach(element => {
+                            let diff = Math.abs(element.Final_Time - element.Initial_Time);
+                            invested_hours_on_activity += diff / 3600000;
+                          });
+                          const remaining_hours_for_activity = result2[0].Estimated_Hours - invested_hours_on_activity;
+                          const a = this.moment(new Date());
+                          const b = this.moment(result1[0].Final_Time);
+                          const c = b.diff(a, 'days');
+                          console.log(c);
+                          const initial_weeknd = (6 - a.day() > c) ? 0 : 1;
+                          let weekends_amount = (parseInt((c - (6 - a.day())) / 7) + initial_weeknd) * 2;
+                          weekends_amount = (b.day() == 5) ? weekends_amount - 1 : weekends_amount;
+                          const working_hours = (c - weekends_amount) * 8;
 
-                        const date = (new Date()).toISOString();
-                        const insertion_query_4 = "SELECT * FROM advance WHERE Initial_Time > '" + this.moment(new Date(date)).format("YYYY-MM-DD HH:mm:ss") + "' AND Final_Time < '" + this.moment(new Date(final_time)).format("YYYY-MM-DD HH:mm:ss") + "' AND Activity_Assignment_Id IN (SELECT Activity_Assignment_Id FROM Activity_Assignment WHERE User_Id = " + result0[0].User_Id + ")";
-                        this.db_connection.query(insertion_query_4, (err4, result4, fields4) => {
-                          if (err4) {
-                            console.log(err4);
-                          } else {
-                            let future_working_hours = 0;
-                            result4.forEach(element => {
-                              let diff = Math.abs(element.Final_Time - element.Initial_Time);
-                              future_working_hours += diff / 3600000;
-                            });
-                            const future_free_time = working_hours - future_working_hours;
-                            const initial_hour = this.moment(new Date(advance_info.initial_hour)).format("YYYY-MM-DD HH:mm:ss");
-                            const final_hour = this.moment(new Date(advance_info.final_hour)).format("YYYY-MM-DD HH:mm:ss");
-                            if(!(remaining_hours_for_activity > future_free_time)) {
-                            const insertion_query_6 = "SELECT Activity_Assignment_Id FROM advance WHERE Advance_Id = " + advance_id;
-                            this.db_connection.query(insertion_query_6, (err5, result5, fields5) => {
-                              if (err5) {
-                                throw err5
-                              } else {
-                                const insertion_query_7 = "REPLACE INTO ADVANCE (Advance_Id, Activity_Assignment_Id, Advance_Comments, Initial_Time, Final_Time) VALUES (" + advance_id + ", " + result1[0].Activity_Assignment_Id + ", '" + advance_comments + "', '" + initial_hour + "', '" + final_hour + "')";
-                                this.db_connection.query(insertion_query_7, (err6, result6, fields6) => {
-                                  if (err6) {
-                                    throw err6
+                          const date = (new Date()).toISOString();
+                          const insertion_query_4 = "SELECT * FROM advance WHERE Initial_Time > '" + this.moment(new Date(date)).format("YYYY-MM-DD HH:mm:ss") + "' AND Final_Time < '" + this.moment(new Date(final_time)).format("YYYY-MM-DD HH:mm:ss") + "' AND Activity_Assignment_Id IN (SELECT Activity_Assignment_Id FROM Activity_Assignment WHERE User_Id = " + result0[0].User_Id + ")";
+                          this.db_connection.query(insertion_query_4, (err4, result4, fields4) => {
+                            if (err4) {
+                              console.log(err4);
+                            } else {
+                              let future_working_hours = 0;
+                              result4.forEach(element => {
+                                let diff = Math.abs(element.Final_Time - element.Initial_Time);
+                                future_working_hours += diff / 3600000;
+                              });
+                              const future_free_time = working_hours - future_working_hours;
+                              const initial_hour = this.moment(new Date(advance_info.initial_hour)).format("YYYY-MM-DD HH:mm:ss");
+                              const final_hour = this.moment(new Date(advance_info.final_hour)).format("YYYY-MM-DD HH:mm:ss");
+                              if (!(remaining_hours_for_activity > future_free_time)) {
+                                const insertion_query_6 = "SELECT Activity_Assignment_Id FROM advance WHERE Advance_Id = " + advance_id;
+                                this.db_connection.query(insertion_query_6, (err5, result5, fields5) => {
+                                  if (err5) {
+                                    throw err5
                                   } else {
-                                    res.send({ warning: (remaining_hours_for_activity > future_free_time) });
+                                    const insertion_query_7 = "REPLACE INTO ADVANCE (Advance_Id, Activity_Assignment_Id, Advance_Comments, Initial_Time, Final_Time) VALUES (" + advance_id + ", " + result1[0].Activity_Assignment_Id + ", '" + advance_comments + "', '" + initial_hour + "', '" + final_hour + "')";
+                                    this.db_connection.query(insertion_query_7, (err6, result6, fields6) => {
+                                      if (err6) {
+                                        throw err6
+                                      } else {
+                                        res.send({ warning: (remaining_hours_for_activity > future_free_time) });
+                                      }
+                                    });
                                   }
                                 });
+                              } else {
+                                res.send({ warning: (remaining_hours_for_activity > future_free_time) });
                               }
-                            });
-                            } else {
-                              res.send({ warning: (remaining_hours_for_activity > future_free_time) });
                             }
-                          }
-                        });
-                      }
-                    });
-                  }
-                });
-              }
-            });
-          }
+                          });
+                        }
+                      });
+                    }
+                  });
+                }
+              });
+            }
           }
         });
       }
